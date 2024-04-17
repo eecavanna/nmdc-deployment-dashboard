f58cbdfd579ef18556ad2b70131ccbc614fb8b50
eecavanna <eecavanna@users.noreply.github.com>
2024-04-16 21:05:01 -0700

    Add button that toggles between light/dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { FetchProvider } from "fetch-react-hook";
+import ThemeSelector from "./components/ThemeSelector.tsx";
 
 function App() {
   return (
     <FetchProvider>
       <Container className={"py-5"}>
+        <Row>
+          <Col className={"text-end"}>
+            <ThemeSelector />
+          </Col>
+        </Row>
         <Row>
           <Col>
             <h1>Deployment Dashboard</h1>
---
+++ src/components/ThemeSelector.tsx
@@ -0,0 +1,50 @@
+import Button from "react-bootstrap/Button";
+import React, { useEffect, useState } from "react";
+import {
+  Moon as DarkModeIcon,
+  Sun as LightModeIcon,
+} from "react-bootstrap-icons";
+
+// Reference: https://getbootstrap.com/docs/5.3/customize/color-modes/#dark-mode
+const THEME_ATTRIBUTE_NAME = "data-bs-theme";
+
+enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
+interface Props {}
+
+const ThemeSelector: React.FC<Props> = () => {
+  const [selectedTheme, setSelectedTheme] = useState<Theme>(Theme.DARK);
+
+  // Whenever the selected theme changes, update the HTML attribute on the `<html>` tag.
+  // Note: This function directly accesses the DOM, ignoring the fact that we're in a React app.
+  useEffect(() => {
+    document.documentElement.setAttribute(THEME_ATTRIBUTE_NAME, selectedTheme);
+  }, [selectedTheme]);
+
+  const isSelectedThemeDark = selectedTheme === Theme.DARK;
+
+  return (
+    <Button
+      onClick={() =>
+        setSelectedTheme(isSelectedThemeDark ? Theme.LIGHT : Theme.DARK)
+      }
+      variant={"link"}
+      size={"sm"}
+      className={"p-0 align-text-bottom"}
+      title={
+        isSelectedThemeDark ? "Switch to light mode" : "Switch to dark mode"
+      }
+    >
+      {isSelectedThemeDark ? (
+        <DarkModeIcon className={"text-body"} />
+      ) : (
+        <LightModeIcon className={"text-body"} />
+      )}
+    </Button>
+  );
+};
+
+export default ThemeSelector;
