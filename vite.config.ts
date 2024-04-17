7fcf60b119240ea7df96da13a57948deb619791c
eecavanna <eecavanna@users.noreply.github.com>
2024-04-17 00:17:07 -0700

    Configure Vite to accommodate GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Define a base in preparation for deploying to GitHub Pages.
+  // Reference: https://vitejs.dev/guide/static-deploy#github-pages
+  base: "/nmdc-deployment-dashboard/",
 });
